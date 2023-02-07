106f1e955750d7611a9f7f8de0246d1492dadd84
bjawebos <bjawebos@adelberg-online.de>
2023-02-07 21:07:44 +0100

    remove error on events page and switch to container class

--- src/app/services/event.service.ts
+++ src/app/services/event.service.ts
@@ -1,15 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Events } from './events';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventService {
 
-  //eventUrl = "https://www.kaffeesatz-chemnitz.de/events.php";
-  eventUrl = "https://preview.kaffeesatz-chemnitz.de/events.php";
-  //eventUrl = "http://localhost:4200/assets/events.php";
+  eventUrl = environment.eventhost + "/events.php";
 
   constructor(private http: HttpClient) {}
 
